8647c63da5c186c2f45d595f7b0a35c96217e271
Kanchangehu <150506237+Kanchangehu@users.noreply.github.com>
2023-11-26 00:15:54 +0530

    Add files via upload

---
+++ forget.php
@@ -0,0 +1,53 @@
+<?php
+
+    $server = 'localhost';
+    $user_name = 'root';
+    $password = '';
+    $dbname = 'itbasket';
+
+    $connect = mysqli_connect($server,$user_name,$password,$dbname);
+
+    if(!$connect){
+        die("failed" .mysqli_connect_error());
+    }
+
+?>
+
+
+<?php
+    if($_SERVER["REQUEST_METHOD"]=='POST'){
+        $email= $_POST['email'];
+        $current_password= $_POST['current_password'];
+        $new_password= $_POST['new_password'];
+
+
+        $sql ="SELECT * FROM form  WHERE email = '$email'";
+        $result = $connect->query($sql);
+
+        if($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+            
+            if($current_password===$row['password']){
+                $sql= "UPDATE form  SET password='$new_password' WHERE email= '$email'";
+                $result = mysqli_query($connect,$sql);
+
+
+                if($result){
+                    echo "successful";
+                }
+                else{
+                    echo "error";
+                }
+            }
+            else{
+                echo "wrong current password";
+            }
+        
+        }
+        else{
+            echo "email doesn't exist";
+        }
+    }
+    
+    mysqli_close($connect);
+?>
